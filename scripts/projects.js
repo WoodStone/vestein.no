d6d922fff0620fb275ccf6f95e1ed9f256ab348c
WoodStone <vesteindahl@hotmail.com>
2016-01-16 01:31:21 +0100

    Adds partially support for WebP

--- scripts/projects.js
+++ scripts/projects.js
@@ -10,10 +10,20 @@ $(function() {
     });
 });
 
+var cellClass = 'mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone';
+var cardClass = 'mdl-card von-card mdl-shadow--2dp';
+var mediaClass = 'mdl-card__media';
+var titleClass = 'mdl-card__title';
+var titleH2Class = 'mdl-card__title-text';
+var textClass = 'mdl-card__supporting-text';
+var actionClass = 'mdl-card__actions mdl-card--border';
+var actionLinkClass = 'mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect';
+
+var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
 
 function createCell(key, cardData) {
     var cellDiv = document.createElement('div');
-    cellDiv.className = 'mdl-cell mdl-cell--3-col mdl-cell--4-col-tablet mdl-cell--4-col-phone';
+    cellDiv.className = cellClass;
 
     cellDiv.appendChild(createCard(key, cardData));
 
@@ -22,7 +32,7 @@ function createCell(key, cardData) {
 
 function createCard(key, cardData) {
     var cardDiv = document.createElement('div');
-    cardDiv.className = 'mdl-card von-card mdl-shadow--2dp';
+    cardDiv.className = cardClass;
 
     cardDiv.appendChild(createMediaElement(key, cardData.media));
     cardDiv.appendChild(createTitleElement(cardData.title));
@@ -35,9 +45,9 @@ function createCard(key, cardData) {
 function createMediaElement(key, media) {
     var mediaDiv = document.createElement('div');
 
-    mediaDiv.className = 'mdl-card__media';
+    mediaDiv.className = mediaClass;
     mediaDiv.id = key;
-    mediaDiv.style.backgroundImage = 'url("images/' + media + '")';
+    mediaDiv.style.backgroundImage = 'url("images/' + (isSafari ? media.png : media.webp) + '")';
     mediaDiv.style.backgroundPosition = 'center';
     mediaDiv.style.backgroundSize = 'cover';
 
@@ -46,10 +56,10 @@ function createMediaElement(key, media) {
 
 function createTitleElement(title) {
     var titleDiv = document.createElement('div');
-    titleDiv.className = 'mdl-card__title';
+    titleDiv.className = titleClass;
 
     var h2 = document.createElement('h2');
-    h2.className = 'mdl-card__title-text';
+    h2.className = titleH2Class;
     h2.innerHTML = title;
 
     titleDiv.appendChild(h2);
@@ -59,7 +69,7 @@ function createTitleElement(title) {
 
 function createTextElement(text) {
     var textDiv = document.createElement('div');
-    textDiv.className = 'mdl-card__supporting-text';
+    textDiv.className = textClass;
     textDiv.innerHTML = text;
 
     return textDiv;
@@ -67,11 +77,11 @@ function createTextElement(text) {
 
 function createActionElement(buttons) {
     var actionDiv = document.createElement('div');
-    actionDiv.className = 'mdl-card__actions mdl-card--border';
+    actionDiv.className = actionClass;
 
     $.each(buttons, function(key, ref) {
         var link = document.createElement('a');
-        link.className = 'mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect';
+        link.className = actionLinkClass;
         link.href = ref;
         link.innerHTML = key;
 
